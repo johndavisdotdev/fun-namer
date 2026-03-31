ecaaeeb77acc0f9c9fe9bfc42bf754cea80b82a2
steve-lead-eng-agent <steve@agents.hitchpin.dev>
2026-03-30 19:14:02 -0500

    Updated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using FunNamer;
+
+string name = string.Empty;
+do
+{
+    var adj = Adj.Random();
+    var n = Names.Random();
+    name = $"{adj}_{n}";
+} while (name == "boring_wozniak");
+// As noted in the <a href="https://github.com/moby/moby/blob/master/internal/namesgenerator/names-generator.go#L852">original</a>,
+// Steve Wozniak is not boring
+Console.WriteLine(name);
